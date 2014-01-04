4b9f3a7020d3872973d63cde4e7302d9917cc3c8
Nat Burns <nbaccount@burnskids.com>
2014-01-04 13:36:05 -0500

    Add non-forking option when debugging.
    On Unix, the USR1 process signal is used to initiate debugging, while
    on Windows we use the undocumented Node API `process._debugProcess()`.

--- tasks/debug.js
+++ tasks/debug.js
@@ -2,27 +2,47 @@ var child_process = require('child_process');
 
 module.exports = function(grunt) {
 	grunt.registerTask('debug', function(type) {
-		grunt.task.clearQueue();
-		
-		var args = process.argv;
-		var done = this.async();
-		
-		var child = child_process.fork(
-			args[1],
-			args.slice(
-				args.indexOf(this.nameArgs) + 1
-			),
-			{
-				execArgv: [
-					type === 'break'
-						? '--debug-brk'
-						: '--debug'
-				]
-			}
-		);
-		
-		child.on('exit', function(code) {
-			done(code == 0);
+		var options = this.options({
+			fork: false
 		});
+		
+		if (options.fork) {
+			// Manually fork Grunt process for debugging
+			
+			grunt.task.clearQueue();
+			
+			var args = process.argv;
+			var done = this.async();
+			
+			var child = child_process.fork(
+				args[1],
+				args.slice(
+					args.indexOf(this.nameArgs) + 1
+				),
+				{
+					execArgv: [
+						type === 'break'
+							? '--debug-brk'
+							: '--debug'
+					]
+				}
+			);
+			
+			child.on('exit', function(code) {
+				done(code == 0);
+			});
+		}
+		else {
+			// Debug existing Grunt process
+			
+			var pid = process.pid;
+			
+			if (process.platform === 'win32') {
+				process._debugProcess(pid);
+			}
+			else {
+				process.kill(pid, 'SIGUSR1');
+			}
+		}
 	});
 };
