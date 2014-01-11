9f87de1da6be4d0e23a64d9ee78154cfe6eabe50
Nat Burns <nbaccount@burnskids.com>
2014-01-11 17:41:45 -0500

    Add option to automatically start a node-inspectory instance.

--- tasks/debug.js
+++ tasks/debug.js
@@ -1,48 +1,169 @@
 var child_process = require('child_process');
+var path = require('path');
+var Q = require('q');
 
 module.exports = function(grunt) {
-	grunt.registerTask('debug', function(type) {
-		var options = this.options({
-			fork: false
+	/**
+	 * Manually forks the Grunt process for debugging
+	 */
+	var forkGrunt = function(brk, name) {
+		var deferred = Q.defer();
+		var args = process.argv;
+		
+		grunt.task.clearQueue();
+		
+		var child = child_process.fork(
+			args[1],
+			args.slice(
+				args.indexOf(name) + 1
+			),
+			{
+				execArgv: [
+					brk
+						? '--debug-brk'
+						: '--debug'
+				]
+			}
+		);
+		
+		child.on('exit', function(code) {
+			if (code == 0) {
+				deferred.resolve();
+			}
+			else {
+				deferred.reject(code);
+			}
 		});
 		
-		if (options.fork) {
-			// Manually fork Grunt process for debugging
-			
-			grunt.task.clearQueue();
-			
-			var args = process.argv;
-			var done = this.async();
-			
-			var child = child_process.fork(
-				args[1],
-				args.slice(
-					args.indexOf(this.nameArgs) + 1
-				),
-				{
-					execArgv: [
-						type === 'break'
-							? '--debug-brk'
-							: '--debug'
-					]
-				}
-			);
-			
-			child.on('exit', function(code) {
-				done(code == 0);
-			});
-		}
-		else {
-			// Debug existing Grunt process
-			
-			var pid = process.pid;
-			
+		return deferred.promise;
+	};
+	
+	/**
+	 * Sets up existing Grunt process for debugging
+	 */
+	var debugGrunt = function(brk) {
+		var pid = process.pid;
+		
+		return Q.fcall(function() {
 			if (process.platform === 'win32') {
 				process._debugProcess(pid);
 			}
 			else {
 				process.kill(pid, 'SIGUSR1');
 			}
+		});
+	};
+	
+	/**
+	 * Initializes a node-inspector instance
+	 */
+	var startInspector = function(params, requireConnect) {
+		var deferred = Q.defer();
+		
+		try {
+			require.resolve('node-inspector/bin/inspector');
 		}
+		catch (e) {
+			deferred.reject('Unable to locate node-inspector package.');
+			return deferred.promise;
+		}
+		
+		var args = [];
+		
+		for (var key in params) {
+			args.push('--' + key, params[key]);
+		}
+		
+		var child = child_process.fork(
+			path.join(__dirname, 'lib/inspector.js'),
+			args
+		);
+		
+		child.on(
+			'message',
+			function(data) {
+				if (data.event === 'SERVER.LAUNCHING') {
+					console.log('Node Inspector v%s', data.version);
+				}
+				else if (data.event === 'SERVER.LISTENING') {
+					console.log('Visit %s to start debugging.', data.address.url);
+					
+					if (!requireConnect) {
+						deferred.resolve(data.address);
+					}
+				}
+				else if (data.event === 'SERVER.CONNECTED') {
+					if (requireConnect) {
+						deferred.resolve();
+					}
+				}
+				else if (data.event === 'SERVER.ERROR') {
+					deferred.reject(data.error);
+				}
+			}
+		);
+		
+		child.on(
+			'exit',
+			function(code) {
+				if (code != 0) {
+					deferred.reject(code);
+				}
+			}
+		);
+		
+		return deferred.promise;
+	};
+	
+	grunt.registerTask('debug', function(type) {
+		var done = this.async();
+		
+		var options = this.options({
+			fork: false,
+			inspector: false,
+			inspectorArgs: {}
+		});
+		
+		var nameArgs = this.nameArgs;
+		var brk = (type === 'break');
+		
+		Q()
+			.then(function() {
+				if (options.fork) {
+					return Q.all([
+						forkGrunt(brk, nameArgs),
+						options.inspector
+							? startInspector(options.inspectorArgs, false)
+							: null
+					]);
+				}
+				else {
+					return (
+						debugGrunt(brk)
+							.then(function() {
+								if (options.inspector) {
+									return startInspector(options.inspectorArgs, brk);
+								}
+							})
+							.then(function() {
+								if (brk) {
+									debugger;
+								}
+							})
+					);
+				}
+			})
+			.done(
+				function() {
+					done();
+				},
+				function(error) {
+					var status = error instanceof Error
+						? error
+						: false;
+					
+					done(status);
+				}
+			);
 	});
 };
---
+++ tasks/lib/inspector.js
@@ -0,0 +1,82 @@
+var fs = require('fs');
+var path = require('path');
+
+var DebugServer = require('node-inspector/lib/debug-server.js').DebugServer;
+var config = require('node-inspector/lib/config.js');
+var packageJson = require('node-inspector/package.json');
+
+var notifyParentProcess = getNotifyParentProcessFn();
+
+notifyParentProcess({
+	event: 'SERVER.LAUNCHING',
+	version: packageJson.version
+});
+
+var debugServer = new DebugServer();
+
+debugServer.on('error', onError);
+debugServer.on('listening', onListening);
+
+debugServer.on(
+	'close',
+	function() {
+		process.exit();
+	}
+);
+
+debugServer.start(config);
+
+debugServer.wsServer.sockets.on(
+	'connection',
+	function(connection) {
+		connection.on('message', function(data) {
+			var message = JSON.parse(data);
+			
+			if (message.method === 'Runtime.enable') {
+				notifyParentProcess({
+					event: 'SERVER.CONNECTED'
+				});
+			}
+		});
+	}
+);
+
+function onError(err) {
+	console.error(
+		'Cannot start the server at %s:%s. Error: %s.',
+		config.webHost || '0.0.0.0',
+		config.webPort,
+		err.message || err
+	);
+
+	if (err.code === 'EADDRINUSE') {
+		console.error(
+			'There is another process already listening at this address.\n' +
+			'Run `node-inspector --web-port={port}` to use a different port.'
+		);
+	}
+
+	notifyParentProcess({
+		event: 'SERVER.ERROR',
+		error: err
+	});
+}
+
+function onListening() {
+	var address = this.address();
+	
+	notifyParentProcess({
+		event: 'SERVER.LISTENING',
+		address: address
+	});
+}
+
+function getNotifyParentProcessFn() {
+	if (!process.send) {
+		return function(msg) {};
+	}
+
+	return function(msg) {
+		process.send(msg);
+	};
+}
